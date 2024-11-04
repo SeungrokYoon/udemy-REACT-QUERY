a00fd9f675007b55352d267ec7f0cf6c6eecce89
SeungrokYoon <sonicrok93@gmail.com>
2024-11-05 08:46:15 +0900

    :sparkles: Feat :  use dependency array for creating dynamic keys

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { useState } from "react";
 
@@ -55,15 +56,19 @@ export function useAppointments() {
   // useQuery call for appointments for the current monthYear
 
   // TODO: update with useQuery!
+
   // Notes:
   //    1. appointments is an AppointmentDateMap (object with days of month
   //       as properties, and arrays of appointments for that day as values)
   //
   //    2. The getAppointments query function needs monthYear.year and
   //       monthYear.month
-  const appointments: AppointmentDateMap = {};
+  const fallback: AppointmentDateMap = {};
 
   /** ****************** END 3: useQuery  ******************************* */
-
+  const { data: appointments = fallback } = useQuery({
+    queryKey: [queryKeys.appointments, monthYear.year, monthYear.month],
+    queryFn: () => getAppointments(monthYear.year, monthYear.month),
+  });
   return { appointments, monthYear, updateMonthYear, showAll, setShowAll };
 }
