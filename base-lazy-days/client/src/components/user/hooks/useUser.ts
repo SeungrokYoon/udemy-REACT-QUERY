e5a87b0d650bc3cd60007e0de18f8e93ff639906
SeungrokYoon <sonicrok93@gmail.com>
2024-11-08 09:47:58 +0200

    :sparkles: Feat :  update and remove queries with setQueryData & removeQueries

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
@@ -22,6 +22,7 @@ async function getUser(userId: number, userToken: string) {
 
 export function useUser() {
   // TODO: call useQuery to update user data from server
+  const queryClient = useQueryClient();
   //get details of userID
   const { userId, userToken } = useLoginData();
 
@@ -36,11 +37,16 @@ export function useUser() {
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
+    queryClient.setQueryData(
+      generateUserKey(newUser.id, newUser.token),
+      newUser
+    );
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    queryClient.removeQueries({ queryKey: [queryKeys.user] });
   }
 
   return { user, updateUser, clearUser };
